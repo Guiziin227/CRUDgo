e5c0f34ea32a0014016faff40ca3ccc83c80f7ca
Guiziin227 <gh63388@gmail.com>
2025-07-24 13:40:43 -0300

    initialize gin router and set up routes

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/guiziin227/CRUDgo/src/controller/routes"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -13,4 +15,11 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	fmt.Println(os.Getenv("TEST"))
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
