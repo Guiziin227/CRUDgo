2c895260fa3cc8e74d69eab79c0825ac4a7a9471
Guiziin227 <gh63388@gmail.com>
2025-07-24 13:53:00 -0300

    implement CreateUser function with JSON binding and error handling

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,20 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/guiziin227/CRUDgo/src/configuration/c_err"
+	"github.com/guiziin227/CRUDgo/src/controller/model/request"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		cErr := c_err.NewBadRequestErr(fmt.Sprintf("Invalid request body: %v\n", err.Error()))
+		c.JSON(cErr.Code, cErr)
+	}
+
+	fmt.Println(userRequest)
+}
