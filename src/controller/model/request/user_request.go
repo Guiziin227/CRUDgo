66b36f924d54445966e5a35010c9eb9985fc9bd3
Guiziin227 <gh63388@gmail.com>
2025-07-25 10:30:17 -0300

    update user request validation to enforce password complexity and require username

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -2,7 +2,7 @@ package request
 
 type UserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=4,max=32"`
-	Username string `json:"username"`
+	Password string `json:"password" binding:"required,min=4,containsany=!@#$%*_"`
+	Username string `json:"username" binding:"required,min=4,max=100"`
 	Age      int8   `json:"age" binding:"required,gt=0"`
 }
