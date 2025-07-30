3f08824e1cffec81bb5ae4ccad08867f75d0f48d
Guiziin227 <gh63388@gmail.com>
2025-07-30 10:50:15 -0300

    add logging for CreateUser controller and improve user response handling

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,21 +1,33 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/guiziin227/CRUDgo/src/configuration/logger"
 	"github.com/guiziin227/CRUDgo/src/configuration/validation"
 	"github.com/guiziin227/CRUDgo/src/controller/model/request"
+	"github.com/guiziin227/CRUDgo/src/controller/model/response"
+	"go.uber.org/zap"
+	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-
+	logger.Info("Initializing CreateUser controller", zap.String("method", "CreateUser"))
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user request", err, zap.String("method", "CreateUser"))
 		cErr := validation.ValidateUserError(err)
 		c.JSON(cErr.Code, cErr)
 		return
 	}
 
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		Email:    userRequest.Email,
+		Username: userRequest.Username,
+		Age:      userRequest.Age,
+	}
+
+	logger.Info("Create user successfully", zap.String("method", "CreateUser"))
+
+	c.JSON(http.StatusOK, response)
 }
