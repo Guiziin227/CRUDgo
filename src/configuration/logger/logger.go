a926a2ff3748e4ca71c850fd807b55f665dd5ced
Guiziin227 <gh63388@gmail.com>
2025-07-30 10:37:23 -0300

    initialize logger with configurable output and log level

--- src/configuration/logger/logger.go
+++ src/configuration/logger/logger.go
@@ -1,5 +1,57 @@
 package logger
 
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"strings"
+)
+
+var (
+	log *zap.Logger
+
+	LOG_OUTPUT = os.Getenv("LOG_OUTPUT")
+	LOG_LEVEL  = os.Getenv("LOG_LEVEL")
+)
+
 func init() {
 
+	logConfig := zap.Config{
+		OutputPaths: []string{getOutputLog()},
+		Level:       zap.NewAtomicLevelAt(getLevelLogs()),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			// Define the keys for the log fields
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "message",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	log, _ = logConfig.Build()
+}
+
+func getOutputLog() string {
+	output := strings.ToLower(strings.TrimSpace(LOG_OUTPUT))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
+func getLevelLogs() zapcore.Level {
+	level := strings.ToLower(strings.TrimSpace(LOG_LEVEL))
+	switch level {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
 }
