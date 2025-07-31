ba4aac4b53d0361a7e2eae837510a853c7614d05
Guiziin227 <gh63388@gmail.com>
2025-07-31 17:27:44 -0300

    add UserDomain methods for retrieving user data and password encryption

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -3,9 +3,18 @@ package model
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"github.com/guiziin227/CRUDgo/src/configuration/c_err"
 )
 
+type UserDomainInterface interface {
+	GetEmail() string
+	GetPassword() string
+	GetUsername() string
+	GetAge() int8
+
+	EncryptPassword()
+}
+
+// NewUserDomain creates a new instance of userDomain with the provided parameters.
 func NewUserDomain(email, password, username string, age int8) *userDomain {
 	return &userDomain{
 		email:    email,
@@ -15,6 +24,7 @@ func NewUserDomain(email, password, username string, age int8) *userDomain {
 	}
 }
 
+// userDomain represents the user domain model with methods to manipulate user data.
 type userDomain struct {
 	email    string
 	password string
@@ -22,16 +32,30 @@ type userDomain struct {
 	age      int8
 }
 
+// GetEmail returns the email of the user.
+func (ud *userDomain) GetEmail() string {
+	return ud.email
+}
+
+// GetPassword returns the password of the user.
+func (ud *userDomain) GetPassword() string {
+	return ud.password
+}
+
+// GetUsername returns the username of the user.
+func (ud *userDomain) GetUsername() string {
+	return ud.username
+}
+
+// GetAge returns the age of the user.
+func (ud *userDomain) GetAge() int8 {
+	return ud.age
+}
+
+// EncryptPassword hashes the user's password using MD5.
 func (ud *userDomain) EncryptPassword() {
 	hash := md5.New()
 	defer hash.Reset()
 	hash.Write([]byte(ud.password))
 	ud.password = hex.EncodeToString(hash.Sum(nil))
-}
-
-type UserDomainInterface interface {
-	CreateUser() *c_err.CErr
-	UpdateUser(string) *c_err.CErr
-	FindUser(string) (*userDomain, *c_err.CErr)
-	DeleteUser(string) *c_err.CErr
 }
