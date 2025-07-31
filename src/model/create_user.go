0c197ba408d1d041a3a60ef3317dd6bdd679ed55
Guiziin227 <gh63388@gmail.com>
2025-07-30 23:10:23 -0300

    add CreateUser method to UserDomain for user creation functionality

---
+++ src/model/create_user.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"fmt"
+	"github.com/guiziin227/CRUDgo/src/configuration/c_err"
+	"github.com/guiziin227/CRUDgo/src/configuration/logger"
+	"go.uber.org/zap"
+)
+
+func (ud *UserDomain) CreateUser() *c_err.CErr {
+
+	logger.Info("Creating user in UserDomain", zap.String("journey", "CreateUser"))
+
+	ud.EncryptPassword()
+
+	fmt.Println(ud)
+
+	return nil
+}
