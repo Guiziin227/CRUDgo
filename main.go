a24fa347df2f87d43ab268566738115742424e0f
Guiziin227 <gh63388@gmail.com>
2025-08-03 21:27:53 -0300

    update main.go to establish MongoDB connection and initialize user repository

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/guiziin227/CRUDgo/src/configuration/db/mongodb"
 	"github.com/guiziin227/CRUDgo/src/configuration/logger"
 	"github.com/guiziin227/CRUDgo/src/controller"
 	"github.com/guiziin227/CRUDgo/src/controller/routes"
+	"github.com/guiziin227/CRUDgo/src/model/repository"
 	"github.com/guiziin227/CRUDgo/src/model/service"
 	"github.com/joho/godotenv"
 	"log"
@@ -17,8 +20,14 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	db, err := mongodb.NewMongoDBConnection(context.Background())
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB: ", err)
+	}
+
 	//Init dependencies
-	service := service.NewUserDomainService()
+	repo := repository.NewUserRepository(db)
+	service := service.NewUserDomainService(repo)
 	controller := controller.NewUserController(service)
 
 	router := gin.Default()
